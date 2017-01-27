5d8c38c1f283f873b5003c717eb09a67cbdef320
novokrest <novokrest013@gmail.com>
2017-01-27 15:11:38 +0300

    [core] CountableCollection implement Iterable elements

--- src/main/java/core/CountableCollection.java
+++ src/main/java/core/CountableCollection.java
@@ -1,6 +1,6 @@
 package core;
 
-public interface CountableCollection<T extends Comparable<T>> {
+public interface CountableCollection<T> extends Iterable<T> {
     void add(T element, int count);
     void remove(T element, int count);
     int count(T element);
--- src/main/java/core/CountableCollectionImpl.java
+++ src/main/java/core/CountableCollectionImpl.java
@@ -1,46 +1,60 @@
 package core;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
-public class CountableCollectionImpl<T extends Comparable<T>> implements CountableCollection<T> {
-    private final Map<T, Integer> elementsCount = new HashMap<T, Integer>();
+public class CountableCollectionImpl<T> implements CountableCollection<T> {
+    private final Map<T, Integer> countForElement;
+
+    public CountableCollectionImpl() {
+        countForElement = new HashMap<>();
+    }
+
+    public CountableCollectionImpl(int initialCapacity) {
+        countForElement = new HashMap<>(initialCapacity);
+    }
 
     public void add(T element, int count) {
         Verifiers.verify(count > 0, "Failed to add zero count of elements");
 
-        if (!elementsCount.containsKey(element)) {
-            elementsCount.put(element, count);
+        if (!countForElement.containsKey(element)) {
+            countForElement.put(element, count);
         }
         else {
-            Integer currentCount = elementsCount.get(element);
-            elementsCount.put(element, currentCount + count);
+            Integer currentCount = countForElement.get(element);
+            countForElement.put(element, currentCount + count);
         }
     }
 
     public void remove(T element, int count) {
         Verifiers.verify(count > 0, "Failed to remove zero count of elements");
 
-        Integer currentCount = elementsCount.get(element);
+        Integer currentCount = countForElement.get(element);
         Verifiers.verify(currentCount >= count, "Failed to remove specified count of element: %d", count);
 
         currentCount -= count;
         if (currentCount > 0) {
-            elementsCount.put(element, currentCount);
+            countForElement.put(element, currentCount);
         }
         else {
-            elementsCount.remove(element);
+            countForElement.remove(element);
         }
     }
 
     public int count(T element) {
-        if (elementsCount.containsKey(element)) {
-            return elementsCount.get(element);
+        if (countForElement.containsKey(element)) {
+            return countForElement.get(element);
         }
         return 0;
     }
 
     public long size() {
-        return elementsCount.size();
+        return countForElement.size();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return countForElement.keySet().iterator();
     }
 }
