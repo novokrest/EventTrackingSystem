d784d1c7493899c76bba6c66123a15d34a815a1c
novokrest <novokrest013@gmail.com>
2017-01-27 15:14:19 +0300

    [core] move queues initialization logic from MultiBlockingQueue

--- src/main/java/core/MultiBlockingQueue.java
+++ src/main/java/core/MultiBlockingQueue.java
@@ -1,31 +1,15 @@
 package core;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class MultiBlockingQueue<E> {
     private final Random random = new Random();
     private final List<BlockingQueue<E>> queues;
 
-    public MultiBlockingQueue(int queuesCount) {
-        this(queuesCount, Integer.MAX_VALUE);
-    }
-
-    public MultiBlockingQueue(int queuesCount, int queueSize) {
-        Verifiers.verifyArg(queuesCount > 0, "queuesCount", queuesCount);
-        Verifiers.verifyArg(queueSize > 0, "queuesSize", queueSize);
-
-        queues = new ArrayList<>(queuesCount);
-        AddQueues(queueSize);
-    }
-
-    private void AddQueues(final int queueSize) {
-        for (int i = 0; i < queueSize; ++i) {
-            queues.add(new LinkedBlockingQueue<E>(queueSize));
-        }
+    public MultiBlockingQueue(List<BlockingQueue<E>> queues) {
+        this.queues = queues;
     }
 
     public void put(E element) throws InterruptedException {
