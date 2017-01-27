f2136a372c52463ba33c773ae712c6bd5691be26
novokrest <novokrest013@gmail.com>
2017-01-27 15:12:30 +0300

    [core] pooling well-known time periods

---
+++ src/main/java/core/WellKnownTimePeriod.java
@@ -0,0 +1,10 @@
+package core;
+
+import java.time.Duration;
+
+public class WellKnownTimePeriod {
+    public static final Duration ONE_SECOND = Duration.ofSeconds(1);
+    public static final Duration ONE_MINUTE = Duration.ofMinutes(1);
+    public static final Duration ONE_HOUR = Duration.ofHours(1);
+    public static final Duration ONE_DAY = Duration.ofDays(1);
+}
