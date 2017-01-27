9f61862ad4cadafe1e0d17c7b26bbf3dba873dc8
novokrest <novokrest013@gmail.com>
2017-01-27 06:55:28 +0300

    [core] CountableCollectionImpl: rename elementCount -> elementsCount

--- src/main/java/core/CountableCollectionImpl.java
+++ src/main/java/core/CountableCollectionImpl.java
@@ -4,43 +4,43 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CountableCollectionImpl<T extends Comparable<T>> implements CountableCollection<T> {
-    private final Map<T, Integer> elementCount = new HashMap<T, Integer>();
+    private final Map<T, Integer> elementsCount = new HashMap<T, Integer>();
 
     public void add(T element, int count) {
         Verifiers.verify(count > 0, "Failed to add zero count of elements");
 
-        if (!elementCount.containsKey(element)) {
-            elementCount.put(element, count);
+        if (!elementsCount.containsKey(element)) {
+            elementsCount.put(element, count);
         }
         else {
-            Integer currentCount = elementCount.get(element);
-            elementCount.put(element, currentCount + count);
+            Integer currentCount = elementsCount.get(element);
+            elementsCount.put(element, currentCount + count);
         }
     }
 
     public void remove(T element, int count) {
         Verifiers.verify(count > 0, "Failed to remove zero count of elements");
 
-        Integer currentCount = elementCount.get(element);
+        Integer currentCount = elementsCount.get(element);
         Verifiers.verify(currentCount >= count, "Failed to remove specified count of element: %d", count);
 
         currentCount -= count;
         if (currentCount > 0) {
-            elementCount.put(element, currentCount);
+            elementsCount.put(element, currentCount);
         }
         else {
-            elementCount.remove(element);
+            elementsCount.remove(element);
         }
     }
 
     public int count(T element) {
-        if (elementCount.containsKey(element)) {
-            return elementCount.get(element);
+        if (elementsCount.containsKey(element)) {
+            return elementsCount.get(element);
         }
         return 0;
     }
 
     public long size() {
-        return elementCount.size();
+        return elementsCount.size();
     }
 }
